86292609647a13d5be02e91db6329439e4746c8b
heisenberg <laercio_ferracini@yahoo.com.br>
2015-11-20 01:43:58 -0200

    Implementando a Consultar de Fornecedores

--- src/main/java/calceus/desktop/interfaces/fornecedor/ConsultarFornecedor.java
+++ src/main/java/calceus/desktop/interfaces/fornecedor/ConsultarFornecedor.java
@@ -16,6 +16,14 @@ import javax.swing.JTable;
 import javax.swing.JTextField;
 import javax.swing.border.EmptyBorder;
 import javax.swing.table.DefaultTableModel;
+import javax.swing.table.TableModel;
+
+import br.com.calceus.ctrl.FornecedorCTRL;
+import br.com.calceus.modelo.Fornecedor;
+
+import java.awt.event.ActionListener;
+import java.util.List;
+import java.awt.event.ActionEvent;
 
 public class ConsultarFornecedor extends JDialog {
 
@@ -27,8 +35,7 @@ public class ConsultarFornecedor extends JDialog {
 	private JTextField textField;
 	private JTextField textField_1;
 	private JTable tabelaFornecedor;
-
-	
+	private DefaultTableModel modelo;
 
 	/**
 	 * Create the dialog.
@@ -39,83 +46,77 @@ public class ConsultarFornecedor extends JDialog {
 		getContentPane().setLayout(new BorderLayout());
 		contentPanel.setBorder(new EmptyBorder(5, 5, 5, 5));
 		getContentPane().add(contentPanel, BorderLayout.CENTER);
-		
+
 		JPanel jpTipoConsulta = new JPanel();
-		
+
 		JScrollPane scrollPane = new JScrollPane();
 		scrollPane.setMinimumSize(new Dimension(17, 17));
 		GroupLayout gl_contentPanel = new GroupLayout(contentPanel);
-		gl_contentPanel.setHorizontalGroup(
-			gl_contentPanel.createParallelGroup(Alignment.LEADING)
-				.addGroup(gl_contentPanel.createSequentialGroup()
-					.addContainerGap()
-					.addGroup(gl_contentPanel.createParallelGroup(Alignment.LEADING)
-						.addComponent(scrollPane, Alignment.TRAILING, GroupLayout.DEFAULT_SIZE, 754, Short.MAX_VALUE)
-						.addComponent(jpTipoConsulta, Alignment.TRAILING, GroupLayout.DEFAULT_SIZE, 714, Short.MAX_VALUE))
-					.addGap(0))
-		);
-		gl_contentPanel.setVerticalGroup(
-			gl_contentPanel.createParallelGroup(Alignment.LEADING)
-				.addGroup(gl_contentPanel.createSequentialGroup()
-					.addContainerGap()
-					.addComponent(jpTipoConsulta, GroupLayout.PREFERRED_SIZE, 27, GroupLayout.PREFERRED_SIZE)
-					.addGap(39)
-					.addComponent(scrollPane, GroupLayout.DEFAULT_SIZE, 247, Short.MAX_VALUE)
-					.addGap(57))
-		);
-		
+		gl_contentPanel.setHorizontalGroup(gl_contentPanel.createParallelGroup(Alignment.LEADING)
+				.addGroup(gl_contentPanel.createSequentialGroup().addContainerGap()
+						.addGroup(gl_contentPanel.createParallelGroup(Alignment.LEADING)
+								.addComponent(scrollPane, Alignment.TRAILING, GroupLayout.DEFAULT_SIZE, 754,
+										Short.MAX_VALUE)
+								.addComponent(jpTipoConsulta, Alignment.TRAILING, GroupLayout.DEFAULT_SIZE, 714,
+										Short.MAX_VALUE))
+						.addGap(0)));
+		gl_contentPanel
+				.setVerticalGroup(gl_contentPanel.createParallelGroup(Alignment.LEADING)
+						.addGroup(gl_contentPanel.createSequentialGroup().addContainerGap()
+								.addComponent(jpTipoConsulta, GroupLayout.PREFERRED_SIZE, 27,
+										GroupLayout.PREFERRED_SIZE)
+								.addGap(39).addComponent(scrollPane, GroupLayout.DEFAULT_SIZE, 247, Short.MAX_VALUE)
+								.addGap(57)));
+
 		tabelaFornecedor = new JTable();
-		tabelaFornecedor.setModel(new DefaultTableModel(
-			new Object[][] {
-			},
-			new String[] {
-				"Código", "Razão Social","CNPJ", "Telefone", "Site", "Obs"
-			}
-		));
+		tabelaFornecedor.setModel(getModelo());
 		scrollPane.setViewportView(tabelaFornecedor);
-		
+
 		JRadioButton rbRazaoSocial = new JRadioButton("Raz\u00E3o Social");
-		
+
 		JRadioButton rbCnpj = new JRadioButton("CNPJ");
-		
+
 		JRadioButton rbTodos = new JRadioButton("Todos");
+		rbTodos.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				//se a tabela não está preenchida, faz a chamada da lista
+				if (verificaTabela()) {
+					FornecedorCTRL ctrl = new FornecedorCTRL();
+					preencheTabela(ctrl.listarFornecedores());
+				}
+			}
+
+		});
 		ButtonGroup bgPesquisa = new ButtonGroup();
 		bgPesquisa.add(rbRazaoSocial);
 		bgPesquisa.add(rbCnpj);
 		bgPesquisa.add(rbTodos);
-		
+
 		textField = new JTextField();
 		textField.setColumns(10);
-		
+
 		textField_1 = new JTextField();
 		textField_1.setColumns(10);
 		GroupLayout gl_jpTipoConsulta = new GroupLayout(jpTipoConsulta);
-		gl_jpTipoConsulta.setHorizontalGroup(
-			gl_jpTipoConsulta.createParallelGroup(Alignment.LEADING)
+		gl_jpTipoConsulta
+				.setHorizontalGroup(
+						gl_jpTipoConsulta.createParallelGroup(Alignment.LEADING)
+								.addGroup(gl_jpTipoConsulta.createSequentialGroup().addContainerGap()
+										.addComponent(rbRazaoSocial).addGap(18)
+										.addComponent(textField, GroupLayout.PREFERRED_SIZE, 170,
+												GroupLayout.PREFERRED_SIZE)
+										.addGap(18).addComponent(rbCnpj).addGap(18)
+										.addComponent(textField_1, GroupLayout.DEFAULT_SIZE, 173, Short.MAX_VALUE)
+										.addGap(18).addComponent(rbTodos).addGap(102)));
+		gl_jpTipoConsulta.setVerticalGroup(gl_jpTipoConsulta.createParallelGroup(Alignment.LEADING)
 				.addGroup(gl_jpTipoConsulta.createSequentialGroup()
-					.addContainerGap()
-					.addComponent(rbRazaoSocial)
-					.addGap(18)
-					.addComponent(textField, GroupLayout.PREFERRED_SIZE, 170, GroupLayout.PREFERRED_SIZE)
-					.addGap(18)
-					.addComponent(rbCnpj)
-					.addGap(18)
-					.addComponent(textField_1, GroupLayout.DEFAULT_SIZE, 173, Short.MAX_VALUE)
-					.addGap(18)
-					.addComponent(rbTodos)
-					.addGap(102))
-		);
-		gl_jpTipoConsulta.setVerticalGroup(
-			gl_jpTipoConsulta.createParallelGroup(Alignment.LEADING)
-				.addGroup(gl_jpTipoConsulta.createSequentialGroup()
-					.addGroup(gl_jpTipoConsulta.createParallelGroup(Alignment.BASELINE)
-						.addComponent(rbRazaoSocial)
-						.addComponent(rbCnpj)
-						.addComponent(textField_1, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)
-						.addComponent(rbTodos)
-						.addComponent(textField, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE))
-					.addContainerGap(GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
-		);
+						.addGroup(gl_jpTipoConsulta.createParallelGroup(Alignment.BASELINE).addComponent(rbRazaoSocial)
+								.addComponent(rbCnpj)
+								.addComponent(textField_1, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE,
+										GroupLayout.PREFERRED_SIZE)
+								.addComponent(rbTodos).addComponent(textField, GroupLayout.PREFERRED_SIZE,
+										GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE))
+						.addContainerGap(GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)));
 		jpTipoConsulta.setLayout(gl_jpTipoConsulta);
 		contentPanel.setLayout(gl_contentPanel);
 		{
@@ -134,5 +135,27 @@ public class ConsultarFornecedor extends JDialog {
 				buttonPane.add(cancelButton);
 			}
 		}
+	}
+
+	// MOdelo da tabela
+	private TableModel getModelo() {
+		Object[] colunas = new Object[] { "Código", "Razão Social", "CNPJ", "Telefone", "Site", "Obs" };
+		modelo = new DefaultTableModel(null, colunas);
+		return modelo;
+	}
+
+	private void preencheTabela(List<Fornecedor> listarFornecedores) {
+		for (Fornecedor f : listarFornecedores) {
+			modelo.addRow(new Object[] { f.getIdFornecedor(), f.getRazaoSocial(), f.getCnpj(), f.getTelefone(),
+					f.getSite(), f.getObs() });
+		}
+
+	}
+	
+	private boolean verificaTabela() {
+		if (modelo.getRowCount() <= 0)
+			return true;
+		else
+			return false;
 	}
 }
