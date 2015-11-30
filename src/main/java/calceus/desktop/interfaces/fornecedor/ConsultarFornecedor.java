e9232317fa7610b95b508a908c5849314d552626
heisenberg <laercio_ferracini@yahoo.com.br>
2015-11-30 11:35:54 -0200

    Alterando fornecedor

--- src/main/java/calceus/desktop/interfaces/fornecedor/ConsultarFornecedor.java
+++ src/main/java/calceus/desktop/interfaces/fornecedor/ConsultarFornecedor.java
@@ -9,6 +9,7 @@ import javax.swing.GroupLayout;
 import javax.swing.GroupLayout.Alignment;
 import javax.swing.JButton;
 import javax.swing.JDialog;
+import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.JRadioButton;
 import javax.swing.JScrollPane;
@@ -51,25 +52,50 @@ public class ConsultarFornecedor extends JDialog {
 
 		JScrollPane scrollPane = new JScrollPane();
 		scrollPane.setMinimumSize(new Dimension(17, 17));
+
+		JButton btnAlterar = new JButton("ALTERAR");
+
+		JButton btnExcluir = new JButton("EXCLUIR");
+		btnExcluir.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				excluirFornecedor(getLinha());
+			}
+			
+		});
 		GroupLayout gl_contentPanel = new GroupLayout(contentPanel);
 		gl_contentPanel.setHorizontalGroup(gl_contentPanel.createParallelGroup(Alignment.LEADING)
 				.addGroup(gl_contentPanel.createSequentialGroup().addContainerGap()
 						.addGroup(gl_contentPanel.createParallelGroup(Alignment.LEADING)
-								.addComponent(scrollPane, Alignment.TRAILING, GroupLayout.DEFAULT_SIZE, 754,
-										Short.MAX_VALUE)
-								.addComponent(jpTipoConsulta, Alignment.TRAILING, GroupLayout.DEFAULT_SIZE, 714,
-										Short.MAX_VALUE))
-						.addGap(0)));
+								.addGroup(gl_contentPanel.createSequentialGroup()
+										.addComponent(btnAlterar, GroupLayout.PREFERRED_SIZE, 105,
+												GroupLayout.PREFERRED_SIZE)
+										.addGap(18)
+										.addComponent(btnExcluir, GroupLayout.PREFERRED_SIZE, 105,
+												GroupLayout.PREFERRED_SIZE)
+										.addGap(547))
+						.addGroup(gl_contentPanel.createSequentialGroup()
+								.addGroup(gl_contentPanel.createParallelGroup(Alignment.LEADING)
+										.addComponent(scrollPane, Alignment.TRAILING, GroupLayout.DEFAULT_SIZE, 775,
+												Short.MAX_VALUE)
+										.addComponent(jpTipoConsulta, Alignment.TRAILING, GroupLayout.DEFAULT_SIZE, 775,
+												Short.MAX_VALUE))
+								.addGap(0)))));
 		gl_contentPanel
 				.setVerticalGroup(gl_contentPanel.createParallelGroup(Alignment.LEADING)
 						.addGroup(gl_contentPanel.createSequentialGroup().addContainerGap()
 								.addComponent(jpTipoConsulta, GroupLayout.PREFERRED_SIZE, 27,
 										GroupLayout.PREFERRED_SIZE)
-								.addGap(39).addComponent(scrollPane, GroupLayout.DEFAULT_SIZE, 247, Short.MAX_VALUE)
-								.addGap(57)));
+								.addGap(39).addComponent(scrollPane, GroupLayout.DEFAULT_SIZE, 252, Short.MAX_VALUE)
+								.addGap(18)
+								.addGroup(gl_contentPanel.createParallelGroup(Alignment.LEADING)
+										.addComponent(btnAlterar, GroupLayout.PREFERRED_SIZE, 28,
+												GroupLayout.PREFERRED_SIZE)
+										.addComponent(btnExcluir, GroupLayout.PREFERRED_SIZE, 28,
+												GroupLayout.PREFERRED_SIZE))
+								.addContainerGap()));
 
 		tabelaFornecedor = new JTable();
-		tabelaFornecedor.setModel(getModelo());
+		tabelaFornecedor.setModel(getTabelaModelo());
 		scrollPane.setViewportView(tabelaFornecedor);
 
 		JRadioButton rbRazaoSocial = new JRadioButton("Raz\u00E3o Social");
@@ -79,7 +105,7 @@ public class ConsultarFornecedor extends JDialog {
 		JRadioButton rbTodos = new JRadioButton("Todos");
 		rbTodos.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent arg0) {
-				//se a tabela não está preenchida, faz a chamada da lista
+				// se a tabela não está preenchida, faz a chamada da lista
 				if (verificaTabela()) {
 					FornecedorCTRL ctrl = new FornecedorCTRL();
 					preencheTabela(ctrl.listarFornecedores());
@@ -125,6 +151,11 @@ public class ConsultarFornecedor extends JDialog {
 			getContentPane().add(buttonPane, BorderLayout.SOUTH);
 			{
 				JButton okButton = new JButton("OK");
+				okButton.addActionListener(new ActionListener() {
+					public void actionPerformed(ActionEvent arg0) {
+						
+					}
+				});
 				okButton.setActionCommand("OK");
 				buttonPane.add(okButton);
 				getRootPane().setDefaultButton(okButton);
@@ -136,14 +167,30 @@ public class ConsultarFornecedor extends JDialog {
 			}
 		}
 	}
+	
+	
 
 	// MOdelo da tabela
-	private TableModel getModelo() {
+	private TableModel getTabelaModelo() {
 		Object[] colunas = new Object[] { "Código", "Razão Social", "CNPJ", "Telefone", "Site", "Obs" };
 		modelo = new DefaultTableModel(null, colunas);
 		return modelo;
 	}
+	public JTable getTabelaFornecedor() {
+		return tabelaFornecedor;
+	}
 
+	public void setTabelaFornecedor(JTable tabelaFornecedor) {
+		this.tabelaFornecedor = tabelaFornecedor;
+	}
+	
+	public DefaultTableModel getModelo() {
+		return modelo;
+	}
+
+	public void setModelo(DefaultTableModel modelo) {
+		this.modelo = modelo;
+	}
 	private void preencheTabela(List<Fornecedor> listarFornecedores) {
 		for (Fornecedor f : listarFornecedores) {
 			modelo.addRow(new Object[] { f.getIdFornecedor(), f.getRazaoSocial(), f.getCnpj(), f.getTelefone(),
@@ -151,11 +198,30 @@ public class ConsultarFornecedor extends JDialog {
 		}
 
 	}
-	
+
 	private boolean verificaTabela() {
 		if (modelo.getRowCount() <= 0)
 			return true;
 		else
 			return false;
 	}
+	private int getLinha() {
+		return tabelaFornecedor.getSelectedRow();
+	}
+	
+	private void excluirFornecedor(int linha) {
+		if(linha < 0){
+			JOptionPane.showMessageDialog(null, "Não há nenhuma linha seleciona ou inexistente.");
+		}else{
+			int cod = (int) tabelaFornecedor.getValueAt(linha, 0);
+			FornecedorCTRL ctrl = new FornecedorCTRL();
+			if(ctrl.excluir(cod)){
+				JOptionPane.showMessageDialog(null, "Excluido");
+			}
+		}
+		//limpa tabela
+		getModelo().setNumRows(0);
+	}
+
+	
 }
